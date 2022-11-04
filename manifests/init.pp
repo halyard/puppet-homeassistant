f20938db095fd2be1d5d98cc6f70b56b8bfdf018
Les Aker <me@lesaker.org>
2022-11-03 21:36:59 -0400

    add backup support

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,17 +2,25 @@
 #
 # @param hostname sets the hostname for grafana
 # @param datadir sets where the data is persisted
-# @param dbdir sets the directory for sqlite storage
 # @param tls_account sets the TLS account config
 # @param tls_challengealias sets the alias for TLS cert
+# @param backup_target sets the target repo for backups
+# @param backup_watchdog sets the watchdog URL to confirm backups are working
+# @param backup_password sets the encryption key for backup snapshots
+# @param backup_environment sets the env vars to use for backups
+# @param backup_rclone sets the config for an rclone backend
 class homeassistant (
   String $hostname,
   String $datadir,
-  String $dbdir,
   String $tls_account,
   Optional[String] $tls_challengealias = undef,
+  Optional[String] $backup_target = undef,
+  Optional[String] $backup_watchdog = undef,
+  Optional[String] $backup_password = undef,
+  Optional[Hash[String, String]] $backup_environment = undef,
+  Optional[String] $backup_rclone = undef,
 ) {
-  file { ["${datadir}/config", "${datadir}/certs", $dbdir]:
+  file { ["${datadir}/config", "${datadir}/certs"]:
     ensure => directory,
   }
 
@@ -38,10 +46,20 @@ class homeassistant (
       '-e TZ=Etc/UTC',
       "-v ${datadir}/config:/config",
       "-v ${datadir}/certs:/ssl",
-      "-v ${dbdir}:/db",
       '--device /dev/ttyUSB0:/dev/ttyUSB0',
       '--device /dev/ttyUSB1:/dev/ttyUSB1',
     ],
     cmd     => '',
+  }
+
+  if $backup_rclone != undef {
+    backup::repo { 'homeassistant':
+      source        => "${datadir}/config",
+      target        => $backup_target,
+      watchdog_url  => $backup_watchdog,
+      password      => $backup_password,
+      environment   => $backup_environment,
+      rclone_config => $backup_rclone,
+    }
   }
 }
